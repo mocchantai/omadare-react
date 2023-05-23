d9facf3977899d92e2f270c4ca37fab5fcf58d85
mocchantai <mocchantai31415@gmail.com>
2023-05-23 18:35:13 +0900

    Reafactor: use assertJsonValidationErrors for validation error #21

--- tests/Feature/FriendTest.php
+++ tests/Feature/FriendTest.php
@@ -61,11 +61,7 @@ class FriendTest extends TestCase
 
         $response
             ->assertStatus(422)//エラーが帰ってきているかをチェックするには422
-            ->assertJsonFragment([//指定した配列が JSON データ内のどこかに部分的に一致するかどうかを確認
-                'errors' => [
-                    'friend_name' => ["名前は必ず指定してください。"]
-                ]
-            ]);
+            ->assertJsonValidationErrors(['friend_name' =>"名前は必ず指定してください。"]);
     }
 
     public function test_friend_update():void
