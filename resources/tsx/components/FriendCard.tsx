f9ef4acc402afb5347d064bac8dbd391a34e96c5
mocchantai <mocchantai31415@gmail.com>
2023-05-25 03:08:14 +0900

    Add: FriendCard #25

--- resources/tsx/components/FriendCard.tsx
+++ resources/tsx/components/FriendCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './_FriendCard.scss';
 
 const FriendCard = () => {
     return (
-        <div>
-            FriendCard
+        <div className="friend_card">
+            <h3 className="friend_card__name">サンプルサンプル</h3>
+            <p className="friend_card__memo">テストテストメモメモメモメモテ</p>
         </div>
     )
 }
