acf445f02327463f060b6cc253b4677f53e508de
mocchantai <mocchantai31415@gmail.com>
2023-05-25 16:06:38 +0900

    Fix: change route / is friendlist

--- resources/tsx/App.tsx
+++ resources/tsx/App.tsx
@@ -7,12 +7,12 @@ const App = () => {
         <BrowserRouter>
             {/*<Link to='/login'>Login</Link><br/>*/}
             {/*<Link to='/registration'>Registration</Link><br/>*/}
-            <Link to='/friendlist'>Friend</Link><br/>
+            {/*<Link to='/friendlist'>Friend</Link><br/>*/}
 
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/registration" element={<RegistrationPage />} />
-                <Route path="/friendlist" element={<FriendListPage />} />
+                <Route path="/" element={<FriendListPage />} />
             </Routes>
         </BrowserRouter>
     );
