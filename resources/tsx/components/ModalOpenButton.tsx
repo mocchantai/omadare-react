2869268ab921b0faf3e50e27094262fdacfef02a
mocchantai <mocchantai31415@gmail.com>
2023-05-25 22:17:14 +0900

    Add: add onClick event on ModalOpenButton and pass that event to parent component; ModalOpenButton. when you click (a) tag in ModalOpenButton.tsx the function toggleModal will activate and open Modal

--- resources/tsx/components/ModalOpenButton.tsx
+++ resources/tsx/components/ModalOpenButton.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './_ModalOpenButton.scss';
 
-const ModalOpenButton = () => {
+
+type ModalOpenButtonProps = {
+    onClick: () => void;
+};
+
+const ModalOpenButton = ({ onClick }: ModalOpenButtonProps) => {
     return (
         <>
-            <a className="modal_open_button" href="#"><img src="/images/plus_icon.svg" alt=""/></a>
+            <a className="modal_open_button" href="#" onClick={onClick}>
+                <img src="/images/plus_icon.svg" alt="" />
+            </a>
         </>
-    )
-}
+    );
+};
 
 
 
--- resources/tsx/pages/FriendListPage.tsx
+++ resources/tsx/pages/FriendListPage.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header, SearchBar, FriendList, ModalOpenButton, FriendCreateModal,} from "../components/index";
 import "./_FriendListPage.scss";
 
 
 const FriendListPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
 
     return(
         <div className="whole_page">
             <Header />
             <SearchBar />
             {/*<FriendList />*/}
-            <FriendCreateModal />
+            {isModalOpen && <FriendCreateModal />}
             {/*<FriendEditModal />*/}
-            <ModalOpenButton />
+            <ModalOpenButton onClick={toggleModal} />
         </div>
-    )
-}
+    );
+};
 
 export default FriendListPage;
