b0e88fe65e6f4b747dbf4e5c82ff1ca5477bf1d0
mocchantai <mocchantai31415@gmail.com>
2023-05-25 15:45:09 +0900

    Add: create FriendCard for FriendList

--- resources/tsx/components/FriendCard.tsx
+++ resources/tsx/components/FriendCard.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import './_FriendCard.scss';
 
-const FriendCard = () => {
+type FriendCardProps = {
+    friend_name: string;
+    memo: string;
+};
+
+const FriendCard: React.FC<FriendCardProps> = ({ friend_name, memo }) => {
     return (
-        <div className="friend_card">
-            <h3 className="friend_card__name">サンプルサンプル</h3>
-            <p className="friend_card__memo">テストテストメモメモメモメモテ</p>
+        <div>
+            <h3>{friend_name}</h3>
+            <p>{memo}</p>
         </div>
-    )
-}
+    );
+};
 
 export default FriendCard;
