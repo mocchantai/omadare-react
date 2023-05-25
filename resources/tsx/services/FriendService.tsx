be76092641ddb48ec66ec07fbd878090799620ad
mocchantai <mocchantai31415@gmail.com>
2023-05-25 15:00:24 +0900

    Add: create fetchFriend. error handling in it

--- resources/tsx/services/FriendService.tsx
+++ resources/tsx/services/FriendService.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import axios from "axios";
 
-const fetchFriends = () => {
-
+const fetchFriends = async () => {
+    try {
+        const response = await axios.get('api/friends');
+        return  response.data;
+    } catch (error) {
+        console.error("failed to fetch friends", error);
+        throw new Error("failed to fetch friends");
+    }
 }
 
 export {
