c98a403b0fefc564011a1aa3321ec2e60a573392
mocchantai <mocchantai31415@gmail.com>
2023-05-28 20:16:30 +0900

    Add: create handleSearch and setkeyword in search bar. and make clean the input when submit #40

--- resources/tsx/components/SearchBar.tsx
+++ resources/tsx/components/SearchBar.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './_SearchBar.scss';
+import {useSearchFriend} from "../hooks";
 
 const SearchBar = () => {
+    const [keyword, setKeyword] = useState("");
+    const { data, isLoading } = useSearchFriend(keyword);
+
+
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(data);
+        console.log(keyword);
+        setKeyword("");
+    }
+
     return (
-        <div className="search_bar">
-            <input type="text" className="search_bar__textbox" placeholder="検索..." />
-            <div className="search_bar__icon">
-                <img src="/images/search_icon.svg" alt="検索アイコン" />
-            </div>
+        <div className="search-bar">
+            <form className="search-form" onSubmit={handleSearch}>
+                <input
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setKeyword(e.target.value)}
+                    type="text"
+                    className="search-form__textbox"
+                    placeholder="検索..."
+                    value={keyword}
+                />
+                <button type="submit" className="search-form__icon">
+                    <img src="/images/search_icon.svg" alt="検索アイコン" />
+                </button>
+            </form>
         </div>
 
     )
