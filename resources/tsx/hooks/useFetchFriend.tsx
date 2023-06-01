791fbff71043b0a84621b1f6104735ed3f641081
mocchantai <mocchantai31415@gmail.com>
2023-06-01 22:30:43 +0900

    Add: add the filter to useFetchFriend.tsx and filter response by context user.id #45

--- resources/tsx/hooks/useFetchFriend.tsx
+++ resources/tsx/hooks/useFetchFriend.tsx
@@ -1,16 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {fetchFriends} from "../services";
 import {FriendType} from "../types";
+import {UserContext} from "../contexts/UserContext";
 
 const useFetchFriend = () => {
     const [data, setData] = useState<FriendType[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const user = useContext(UserContext);
 
 
     const fetchData = async (): Promise<void> => {
         try {
+
+            const loggedInUser = user?.user;
+            console.log("useFetchFriendでContextを見る",loggedInUser?.id);
+
             const response: FriendType[] = await fetchFriends();
-            setData(response);
+            const filteredData = response.filter((friend) => friend.user_id === loggedInUser?.id);
+            setData(filteredData);
+
             setIsLoading(false);
         } catch (error) {
             console.error("Failed to fetch friends", error);
--- resources/tsx/types/UserType.tsx
+++ resources/tsx/types/UserType.tsx
@@ -1,4 +1,5 @@
 type UserType = {
+    id: number;
     name: string;
     email: string;
     password: string;
