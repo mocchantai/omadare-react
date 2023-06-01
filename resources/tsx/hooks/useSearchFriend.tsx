c9b50056d4051480acf74279d8ec76cc9ede1771
mocchantai <mocchantai31415@gmail.com>
2023-06-01 22:54:14 +0900

    Fix: fix the useSearchFriend.tsx too to filter by user_id #45

--- resources/tsx/hooks/useSearchFriend.tsx
+++ resources/tsx/hooks/useSearchFriend.tsx
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {searchFriend} from "../services";
 import {FriendType} from "../types";
+import {UserContext} from "../contexts/UserContext";
 
 const useSearchFriend = (keyword: string) => {
     const [data, setData] = useState<FriendType[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const user = useContext(UserContext);
+
 
     useEffect(() => {
         const searchData = async () => {
             try {
                 const response = await searchFriend(keyword);
-                setData(response);
+                const filteredData = response.filter((friend) => friend.user_id === user?.user?.id);
+                setData(filteredData);
                 setIsLoading(false);
             } catch (error) {
                 console.error("Failed to search friends", error);
