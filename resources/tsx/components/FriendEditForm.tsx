92c72373c84a2ae9aace18d5a54a9e045c56b08b
mocchantai <mocchantai31415@gmail.com>
2023-06-02 11:41:12 +0900

    Add: props destructuring assignment in FriendEditForm.tsx #47

--- resources/tsx/components/FriendEditForm.tsx
+++ resources/tsx/components/FriendEditForm.tsx
@@ -3,28 +3,28 @@ import React, {useContext} from "react";
 import "./_FriendCreateForm.scss";
 import {UserContext} from "../contexts/UserContext";
 
-const FriendEditForm = (props: FriendCreateFormPropsType) => {
+const FriendEditForm = ({onSubmit, onChange, value, onChange1, value1, onClick}: FriendCreateFormPropsType) => {
 
     const user = useContext(UserContext);
 
     return (
-        <form onSubmit={props.onSubmit} className="create_form">
+        <form onSubmit={onSubmit} className="create_form">
             <h4>編集モーダル</h4>
             <input type="hidden" name="user_id" value={user?.user?.id}/>
             <label htmlFor="friend_name">名前</label>
-            <input onChange={props.onChange}
-                   value={props.value} name="friend_name"
+            <input onChange={onChange}
+                   value={value} name="friend_name"
                    type="text"
                    className="create_form__friend_name"
             />
             <label htmlFor="memo">メモ</label>
-            <textarea onChange={props.onChange1}
-                      value={props.value1}
+            <textarea onChange={onChange1}
+                      value={value1}
                       name="memo"
                       className="create_form__memo">
             </textarea>
             <button className="create_form__submit_button">送信</button>
-            <button type="button" className="create_form__close_button" onClick={props.onClick}>閉じる</button>
+            <button type="button" className="create_form__close_button" onClick={onClick}>閉じる</button>
         </form>
     )
 };
